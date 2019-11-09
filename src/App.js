7be5a76a75056020e486fc60e857c69c461909dd
enzodanjour <enzo.lops999@gmail.com>
2019-11-09 11:48:13 -0300

    Add login Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import logo from ".assets/logo.svg";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
     <div className="container">
       <img src={logo} alt="Konnectus" />
-
       <div className="content">
         <p>
           Solucione suas demandas pontuais de forma rápida{" "}
@@ -16,8 +15,10 @@ function App() {
         <form>
           <label htmlFor="user">Usuário</label>
           <input type="user" id="user" placeholder="Usuário" />
+          <br />
           <label htmlFor="password">Senha</label>
           <input type="password" id="password" placeholder="Senha" />
+          <br />
           <button type="submit">Entrar</button>
         </form>
       </div>
