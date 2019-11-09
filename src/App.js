3f93ccb9ae4ddab2130d503bd2a2afd761b23423
enzodanjour <enzo.lops999@gmail.com>
2019-11-09 14:03:59 -0300

    login screen

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import logo from "./assets/logo.svg";
 function App() {
   return (
     <div className="container">
-      <img src={logo} alt="Konnectus" />
+      <img id="logo" src={logo} alt="Konnectus" />
       <div className="content">
         <p>
-          Solucione suas demandas pontuais de forma rápida{" "}
-          <strong>prática</strong> e <strong></strong>eficiente
+          <strong>Menos</strong> dores, <strong>Mais</strong> conexões
         </p>
+
         <form>
           <label htmlFor="user">Usuário</label>
           <input type="user" id="user" placeholder="Usuário" />
           <br />
-          <label htmlFor="password">Senha</label>
+          <label htmlFor="password">Senha </label>
           <input type="password" id="password" placeholder="Senha" />
-          <br />
-          <button type="submit">Entrar</button>
+          <button className="btn" type="submit">
+            Entrar
+          </button>
         </form>
       </div>
     </div>
